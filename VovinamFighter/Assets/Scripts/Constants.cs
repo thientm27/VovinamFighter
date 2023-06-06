507bcf8adc1323a8d46d2a6bf01c08735c57b5a3
thientm27 <denkhotieu1@gmail.com>
2023-06-06 09:44:07 +0700

    player two move

--- VovinamFighter/Assets/Scripts/Constants.cs
+++ VovinamFighter/Assets/Scripts/Constants.cs
@@ -7,5 +7,10 @@
         public static string StraightHand = "StraightHand";
         public static string Kick = "Kick";
         
+        
+        public static string HorizontalTwo = "Horizontal2";
+        public static string VerticalTwo  = "Vertical2";
+        public static string StraightHandTwo  = "StraightHand2";
+        public static string KickTwo  = "Kick2";
     }
 }
--- VovinamFighter/Assets/Scripts/Game/PlayerController.cs
+++ VovinamFighter/Assets/Scripts/Game/PlayerController.cs
@@ -9,15 +9,22 @@ namespace Game
         [SerializeField] private GameModel model;
         [SerializeField] private GameView view;
         [SerializeField] private Transform playerOne;
+        [SerializeField] private Transform playerTwo;
         [SerializeField] private PlayerAnimationController playerAnimationController;
+        [SerializeField] private PlayerAnimationController playerTwoAnimationController;
 
         [SerializeField] public PlayerState playerOneState;
+        [SerializeField] public PlayerState playerTwoState;
 
         private float _timer;
+        private float _timer2;
+        
+        private float _horizontal = 0;
+        private float _horizontal2 = 0;
+
         // Update is called once per frame
         void Update()
         {
-            
             if (Input.GetKeyDown(KeyCode.P))
             {
                 playerOneState = PlayerState.KnockDown;
@@ -46,9 +53,32 @@ namespace Game
                     playerAnimationController.ChangeAnimation(PlayerState.KnockDown);
                     break;
             }
+            
+            switch (playerTwoState)
+            {
+                case PlayerState.Idle:
+                    HandleInputTwo(); // get input for move if idle
+                    break;
+                case PlayerState.MoveUp:
+                    PlayerTwoMove();
+                    break;
+                case PlayerState.MoveDown:
+                    PlayerTwoMove();
+                    break;
+                case PlayerState.StraightHand:
+                    
+                    AttackStraightHandTwo();
+                    break;
+                case PlayerState.Kick:
+                    
+                    AttackKickTwo();
+                    break;
+                case PlayerState.KnockDown:
+                    
+                    playerAnimationController.ChangeAnimation(PlayerState.KnockDown);
+                    break;
+            }
         }
-
-        private float _horizontal = 0;
 
         private void HandleInput()
         {
@@ -83,6 +113,38 @@ namespace Game
 
         }
 
+        private void HandleInputTwo()
+        {
+            playerTwoAnimationController.ChangeAnimation(PlayerState.Idle);
+            _horizontal2 = Input.GetAxisRaw(Constants.HorizontalTwo);
+            _timer2 = 0f;
+            
+            // player move
+            if (Mathf.Abs(_horizontal2) > 0)
+            {
+                playerTwoState = _horizontal2 > 0
+                    ? PlayerState.MoveUp
+                    : PlayerState.MoveDown;
+                return;
+            }
+            
+            // player attack
+            var input = Input.GetAxisRaw(Constants.StraightHandTwo);
+            if (input > 0)
+            {
+                playerTwoState = PlayerState.StraightHand;
+                return;
+            }
+
+            // player attack2
+            var input2 = Input.GetAxisRaw(Constants.KickTwo);
+            if (input2 > 0)
+            {
+                playerTwoState = PlayerState.Kick;
+                return;
+            }
+
+        }
         private void PlayerOneMove()
         {
             _horizontal = Input.GetAxisRaw(Constants.Horizontal);
@@ -99,7 +161,6 @@ namespace Game
             {
                 playerOneState = PlayerState.Idle;
             }
-            
         }
 
         private void AttackStraightHand()
@@ -119,6 +180,43 @@ namespace Game
             if (_timer >= 0.83f)
             {
                 playerOneState = PlayerState.Idle;
+            }
+        }
+         private void PlayerTwoMove()
+        {
+            _horizontal2 = Input.GetAxisRaw(Constants.HorizontalTwo);
+            var newPos = playerTwo.position;
+            newPos.x += _horizontal2 * Time.deltaTime * model.PlayerMoveSpeed;
+            if (Mathf.Abs(_horizontal2) > 0)
+            {
+                playerTwoAnimationController.ChangeAnimation(_horizontal2 > 0
+                    ? PlayerState.MoveUp
+                    : PlayerState.MoveDown);
+                playerTwo.position = newPos;
+            }
+            else
+            {
+                playerTwoState = PlayerState.Idle;
+            }
+        }
+
+        private void AttackStraightHandTwo()
+        {
+            playerTwoAnimationController.ChangeAnimation(PlayerState.StraightHand);
+            _timer2 += Time.deltaTime;
+            if (_timer2 >= 0.5f)
+            {
+                playerTwoState = PlayerState.Idle;
+            }
+        }
+        
+        private void AttackKickTwo()
+        {
+            playerTwoAnimationController.ChangeAnimation(PlayerState.Kick);
+            _timer2 += Time.deltaTime;
+            if (_timer2 >= 0.83f)
+            {
+                playerTwoState = PlayerState.Idle;
             }
         }
         
